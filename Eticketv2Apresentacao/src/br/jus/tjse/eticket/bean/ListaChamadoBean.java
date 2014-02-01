9cfabb918b998fbdffd0aafbc81750f82051f46a
marceloliveira <marcel.ufs@gmail.com>
2014-02-01 16:36:13 -0300

    Mais alterações de chamado

--- Eticketv2Apresentacao/src/br/jus/tjse/eticket/bean/ListaChamadoBean.java
+++ Eticketv2Apresentacao/src/br/jus/tjse/eticket/bean/ListaChamadoBean.java
@@ -4,12 +4,15 @@ import java.util.List;
 
 import javax.faces.bean.ManagedBean;
 
+import br.jus.tjse.eticket.tipo.TipoMensagem;
 import br.jus.tjse.eticket.to.ChamadoTO;
 
 @ManagedBean
 public class ListaChamadoBean {
 	
 	private List<ChamadoTO> chamados;
+	private TipoMensagem tipoMensagem;
+	private String mensagem;
 
 	public List<ChamadoTO> getChamados() {
 		return chamados;
@@ -18,6 +21,26 @@ public class ListaChamadoBean {
 	public void setChamados(List<ChamadoTO> chamados) {
 		this.chamados = chamados;
 	}
+
+	public TipoMensagem getTipoMensagem() {
+		return tipoMensagem;
+	}
+
+	public void setTipoMensagem(TipoMensagem tipoMensagem) {
+		this.tipoMensagem = tipoMensagem;
+	}
+
+	public String getMensagem() {
+		return mensagem;
+	}
+
+	public void setMensagem(String mensagem) {
+		this.mensagem = mensagem;
+	}
 	
+	public static String getStatusChamado(String flStatus) {
+		String status = "";
+		return status;
+	}
 
 }
